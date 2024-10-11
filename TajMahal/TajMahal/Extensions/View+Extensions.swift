f1b5ad53229190d3fd13a7e8ad652718478a8f7a
Fenriz1349 <julien.cotte@live.fr>
2024-10-11 11:46:25 +0200

    add customNavigationTitle

---
+++ TajMahal/TajMahal/Extensions/View+Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  View+Extenstion.swift
+//  TajMahal
+//
+//  Created by Julien Cotte on 11/10/2024.
+//
+
+import Foundation
---
+++ TajMahal/TajMahal/ExtractedView/CustomNavigationTitle.swift
@@ -0,0 +1,18 @@
+//
+//  CustomNavigationTitle.swift
+//  TajMahal
+//
+//  Created by Julien Cotte on 11/10/2024.
+//
+
+import SwiftUI
+
+struct CustomNavigationTitle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomNavigationTitle()
+}
